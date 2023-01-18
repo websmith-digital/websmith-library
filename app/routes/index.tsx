8b595df84af0c6bf36959dff4895785c5d3769cd
Scott Erickson <sryanerickson@gmail.com>
2023-01-18 01:10:56 -0700

    wired in pocketbase

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,24 @@
+import { json } from "@remix-run/node"; // or cloudflare/deno
+import { useLoaderData } from "@remix-run/react";
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("https://api.library.scotterickson.io");
+
+export async function loader() {
+    return json(await pb.collection("pets").getFullList());
+}
+
 export default function Index() {
-    return <h1>Hello remix! changed</h1>;
+    const pets = useLoaderData();
+    return (
+        <>
+            <h1>Hello remix!</h1>
+            <p>Here are some of my pets...</p>
+            <ul>
+                {pets.map(pet => (
+                    <li key={pet.name}>{pet.name}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
